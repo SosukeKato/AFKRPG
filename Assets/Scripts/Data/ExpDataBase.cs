b0c77bdfd68db7e7aa637304086b1f528dbb140d
SosukeKato <dokindokinpiano@gmail.com>
2026-09-14 14:40:17 +0900

    [update]IDからデータを検索できるように変更

--- Assets/Scripts/Data/ExpDataBase.cs
+++ Assets/Scripts/Data/ExpDataBase.cs
@@ -1,18 +1,61 @@
-using System.Collections;
+using System;
 using System.Collections.Generic;
-using UnityEngine;
 
-public class ExpDataBase : MonoBehaviour
+[Serializable]
+public class ExpEntry
 {
-    // Start is called before the first frame update
-    void Start()
+    public string Id;
+    public ExpData Data;
+}
+
+[Serializable]
+public class ExpDataBase
+{
+    //セーブデータ機能に使用
+    public List<ExpEntry> Entries = new();
+
+    [NonSerialized]
+    private Dictionary<string, ExpData> _dict;
+
+    /// <summary>
+    /// ListからDictionaryを作成
+    /// </summary>
+    private void BuildDict()
     {
-        
+        _dict = new Dictionary<string, ExpData>();
+        foreach(ExpEntry exp in Entries)
+            _dict[exp.Id] = exp.Data;
     }
 
-    // Update is called once per frame
-    void Update()
+    /// <summary>
+    /// 指定IDのデータを取得、なければ作成する
+    /// </summary>
+    /// <param name="id"></param>
+    /// <returns></returns>
+    public ExpData GetExpData(string id)
     {
-        
+        if (_dict == null)
+            BuildDict();
+
+        if (!_dict.TryGetValue(id,out ExpData data))
+        {
+            data = new();
+            _dict[id] = data;
+            Entries.Add(new ExpEntry { Id = id, Data = data });
+        }
+        return data;
+    }
+
+    /// <summary>
+    /// 指定IDのデータが存在するか確認
+    /// </summary>
+    /// <param name="id"></param>
+    /// <returns></returns>
+    public bool Exists(string id)
+    {
+        if (_dict == null)
+            BuildDict();
+
+        return _dict.ContainsKey(id);
     }
 }
