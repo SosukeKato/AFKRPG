b326396659841b499a6f7c0eb3f0b445bec45436
SosukeKato <dokindokinpiano@gmail.com>
2026-09-16 08:26:37 +0900

    [update]経験値増加処理追加

--- Assets/Scripts/Manager/ExpManager.cs
+++ Assets/Scripts/Manager/ExpManager.cs
@@ -1,18 +1,36 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+using System;
 
-public class ExpManager : MonoBehaviour
+public class ExpManager
 {
-    // Start is called before the first frame update
-    void Start()
+    public event Action<string, uint, uint> OnLevelUp;
+    public event Action<string, uint, uint> OnExpChanged;
+
+    private readonly ExpDataBase _dataBase;
+
+    public ExpManager(ExpDataBase dataBase)
     {
-        
+        _dataBase = dataBase;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void AddExp(string id, uint amount, uint maxLevel = uint.MaxValue)
     {
-        
+        ExpData data = _dataBase.GetExpData(id);
+        uint oldLevel = data.Level;
+
+        (uint nextLevel, uint remainingExp) = ExpTable.Exp(data.Level, data.Exp + amount, maxLevel);
+
+        data.Level = nextLevel;
+        data.Exp = remainingExp;
+
+        if (nextLevel > oldLevel)
+            OnLevelUp?.Invoke(id, oldLevel, nextLevel);
+
+        OnExpChanged?.Invoke(id, data.Exp, ExpTable.RequiredExp(data.Level));
     }
+
+    public bool Exists(string id) => _dataBase.Exists(id);
+
+    public uint GetLevel(string id) => _dataBase.GetExpData(id).Level;
+
+    public uint GetExp(string id) => _dataBase.GetExpData(id).Exp;
 }
